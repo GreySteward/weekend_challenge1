26857b4dd0a4657d8a3d01ed0a08a360f06f28fe
Chris Gibson <cigibson33@gmail.com>
2016-01-30 15:50:37 -0600

    weekend challenge01

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log("hello world");
+$(document).ready(function() {
+	var empArray = [];
+	var empMonthlySalary = 0;
+	var totalMonthlySalary = 0;
+
+	$('#employeeForm').on('submit', function(event) {
+		event.preventDefault();
+
+		var values = {};
+
+		$.each($('#employeeForm').serializeArray(), function(i, field) {
+			values[field.name] = field.value;
+		});
+
+		empMonthlySalary = parseInt(values.empSalary).toFixed(2)/12;
+		totalMonthlySalary += empMonthlySalary;
+
+		empArray.push(values);
+		console.log(empArray);
+
+		$('#employeeForm').find('input[type=text], input[type=number]').val('');
+		
+		appendDom(values);
+	});
+
+	$('#button').on('click', function(event) {
+		$('#container').find('p').remove();
+		console.log('clear');
+	});
+
+	function appendDom(empInfo) {
+		$('#container').append('<div></div>');
+		var $el = $('#container').children().last();
+
+		$el.append('<p>EmployeeFirstName: ' + empInfo.empFirstName + '</p>');
+		$el.append('<p>EmployeeLastName: ' + empInfo.empLastName + '</p>');
+		$el.append('<p>Employee ID: ' + empInfo.empId + '</p>');
+		$el.append('<p>Employee Job Title: ' + empInfo.empJobTitle + '</p>');
+		$el.append('<p>Employee Salary: ' + empInfo.empSalary + '</p>');
+		$el.append('<p>Employee Monthly Salary: ' + empMonthlySalary + '</p>');
+	}
+});
+
